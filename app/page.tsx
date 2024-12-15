aa43b30ebc86c65484bcb53f35b10f694ffd1394
jason lim <50891910+sxxov@users.noreply.github.com>
2024-12-15 10:43:53 +0000

    Initial commit

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import Hello from "./Hello";
+
+/** Add your relevant code here for the issue to reproduce */
+export default function Home() {
+  return (
+    <main>
+      <Hello />
+    </main>
+  );
+}
---
+++ next.config.mjs
@@ -0,0 +1,11 @@
+/**
+ * @type {import('next').NextConfig}
+ */
+const nextConfig = {
+  reactStrictMode: true,
+  experimental: {
+    reactCompiler: {},
+  },
+};
+
+export default nextConfig;
